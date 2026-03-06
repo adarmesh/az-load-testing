77aa9611d29982bac7f9ab57735866b2cf17d9dc
Aidar Darmesh <darmesh.aidar@gmail.com>
2026-03-06 19:00:58 +0500

    Added staging slot and auto-swapping when load testing is successful

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ az login
 az group create --name az2006-rg --location eastus
 
 # Create webapp
-az appservice plan create --name az2006-plan --resource-group az2006-rg --sku B1
+az appservice plan create --name az2006-plan --resource-group az2006-rg --sku S1
 az webapp create -g az2006-rg -p az2006-plan -n az2006app26168 --runtime "dotnet:8"
+az webapp deployment slot create --name az2006app26168 --resource-group az2006-rg --slot staging
+
 
 # Create load testing resource
 az load create --name az2006loadtest --resource-group az2006-rg --location eastus
