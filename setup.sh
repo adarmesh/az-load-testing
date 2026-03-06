c6e68ec76038baed6f46c0a7313924f0bd97b901
Aidar Darmesh <darmesh.aidar@gmail.com>
2026-03-06 17:03:31 +0500

    Changed deployment slot runtime

--- setup.sh
+++ setup.sh
@@ -5,7 +5,5 @@ az login
 az group create --name az2006-rg --location eastus
 
 # Create webapp
-az appservice plan create --name az2006-plan --resource-group az2006-rg --sku S1
-az webapp create --name az2006app26168 --resource-group az2006-rg --plan az2006-plan
-
-
+az appservice plan create --name az2006-plan --resource-group az2006-rg --sku B1
+az webapp create -g az2006-rg -p az2006-plan -n az2006app26168 --runtime "dotnet:8"
