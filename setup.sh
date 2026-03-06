5f37d02ee9d52d3ec1ebb7b5ec6ea6fbc460dc87
Aidar Darmesh <darmesh.aidar@gmail.com>
2026-03-06 18:02:42 +0500

    Using SP and secret for load testing

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ az group create --name az2006-rg --location eastus
 # Create webapp
 az appservice plan create --name az2006-plan --resource-group az2006-rg --sku B1
 az webapp create -g az2006-rg -p az2006-plan -n az2006app26168 --runtime "dotnet:8"
+
+# Create load testing resource
+az load create --name az2006loadtest --resource-group az2006-rg --location eastus
